e31412beeab250d08a413936c6e66b796277285e
Pedro Rocha <pedrohrrf17@gmail.com>
2021-05-20 16:07:10 -0300

    Projeto Criado

---
+++ src/main/java/com/atendeAi/usjtProj/UsjtProjApplication.java
@@ -0,0 +1,13 @@
+package com.atendeAi.usjtProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsjtProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsjtProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atendeAi/usjtProj/UsjtProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atendeAi.usjtProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsjtProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
